470e4d7020fd966197ac121a658e4e0d20c59c47
Szejker <bartekk.gordon@gmail.com>
2022-12-02 22:04:24 +0100

    Remove require component of rigitbody

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float droneRange;
+    Vector3 mousePosition;
+
+    private void Update()
+    {
+        mousePosition = GetMousePosition();
+
+
+
+        Debug.Log(mousePosition + " " + transform.position);
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, droneRange);
+    }
+
+}
