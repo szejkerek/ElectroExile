9bdddb9c7f0fba2fa1b17f1b0a36414c32917be4
MichalPokrzywa <michpok467@student.polsl.pl>
2022-12-02 21:03:38 +0100

    scripting EnemyBehavior

--- Assets/_Scripts/EnemyBehavior.cs
+++ Assets/_Scripts/EnemyBehavior.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    [SerializeField] bool isMoving;
+    private bool movingLeft;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        if (isMoving)
+        {
+            movingLeft = false;
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMoving)
+        {
+            if (movingLeft == true)
+            {
+                // move left
+                if (transform.position.x <= -4) 
+                    movingLeft = false;
+                else if (transform.position.x >= 4)
+                {
+                    // move right
+                    movingLeft = true;
+                }
+            }
+
+        }
+
+
     }
 }
