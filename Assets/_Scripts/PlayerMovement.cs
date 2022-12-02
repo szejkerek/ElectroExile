4bfccd69d3d3f4e18793c0b3bb4ec9abe2501dfd
Szejker <bartekk.gordon@gmail.com>
2022-12-02 22:05:25 +0100

    fix

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
