2beaeab2f2938ce01d8bb645e9e933b5ef597225
czechumennakompie@gmail.com <czechumennakompie@gmail.com>
2022-12-03 15:32:00 +0100

    Added music without a play method

---
+++ Assets/_Scripts/UI/MusicClass.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicClass : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if(_audioSource.isPlaying) {
+            return;
+        }
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
