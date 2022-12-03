9296ad6bcd2d8a43eb519c1f85660fed321afc55
Biurito <oto.wojtek@gmail.com>
2022-12-03 12:27:07 +0100

    Fix

--- Assets/_Scripts/SpikesBehavior.cs
+++ Assets/_Scripts/SpikesBehavior.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class SpikesBehavior : PlayerManager
+public class SpikesBehavior : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag =="Player")
+        if(collision.GetComponent<Collider2D>().tag =="Player")
         {
             FindObjectOfType<PlayerManager>().KillPlayer();
         }
