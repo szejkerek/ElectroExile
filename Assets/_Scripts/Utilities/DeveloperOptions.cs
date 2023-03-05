8163ba261b4640c35ef6007e236a36f862e80091
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-03-05 20:48:45 +0100

    Fix bug that prevented making builds

--- Assets/_Scripts/Utilities/DeveloperOptions.cs
+++ Assets/_Scripts/Utilities/DeveloperOptions.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class DeveloperOptions
+public class DeveloperOptions : MonoBehaviour
 {
+//Keep all dev options here to be able to create builds
+#if UNITY_EDITOR
+
     private const string _developerFolder = "Developer/";
 
     [MenuItem(_developerFolder + "Player/Teleport to spawnpoint")]
@@ -13,5 +16,7 @@ public class DeveloperOptions
         Vector2 spawnpointPosition = SpawnPoint.Instance.transform.position;
         Player.Instance.transform.position = spawnpointPosition;
     }
+
+#endif
 }
 
